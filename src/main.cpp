ee752fc95ebf7c7c54b3a7835ca41949d93b96ce
TI_Eugene <ti.eugene@gmail.com>
2022-09-16 13:33:58 +0300

    argv[1]|stdin

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 /* TODO:
  * - [x] return new
  * - [x] Sample: vector
- * - [ ] double > float
+ * - [x] ~~double > float~~
+ * - [x] stdin|argv[1]
  * - [ ] SFT: complex
- * - [ ] stdin|argv[1]
  * - [ ] unify functions (wrap)
+ * - [ ] FIXME: handle 'not argv[1] nor stdin'
  */
 #include <vector>
 #include <algorithm>
@@ -30,8 +31,10 @@ double *MeanCount(vector<double>&, int);
 double *RMSCount(vector<double>&, int);
 int Fourier(vector<double>&, int Nwind, double *OutReal, double *OutImag, int Nharm);
 
-int main() {
+int main(int argc, char *argv[]) {
   int leng = 1e6;
+  istream *infile_ptr;
+  ifstream infile;
   vector<double> Samples;
   double *OutReal1 = new double[leng];
   double *OutImag1 = new double[leng];
@@ -49,13 +52,26 @@ int main() {
   double dt = 1000. / 1200;
   int Nwind = 24; // samples per period
 
+  // 00. Prepare input
+  if (argc < 2)
+      infile_ptr = &cin;
+  else {
+      infile.open(argv[1], ios::in);
+      if (!infile.is_open()) {
+          cerr << "Can't open hash file: " << argv[1] << endl;
+          exit(EXIT_FAILURE);
+      }
+      infile_ptr = &infile;
+  }
   // 01. Читаем исходные отсчёты
-  ifstream file("InitSamples.txt");
-  while (getline(file, s)) {
+  while (!getline(*infile_ptr, s).eof()) {
     Samples.push_back(atof(s.c_str()));
     countS++;
   }
-  file.close();
+  if (!countS) {
+      cerr << "No input data" << endl;
+      exit(EXIT_FAILURE);
+  }
   // 02. Расчет среднего значения
   auto OutMean = MeanCount(Samples, Nwind);
   // 03. Расчёт действующего значения
